94a14f4e2987b458f33c3d3688e63e3537d27cff
Carrie Mathieu <carrieburks@gmail.com>
2022-06-07 15:09:29 -0400

    add google map

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 /// <reference types="@types/google.maps" />
 
-import { User } from './User';
-import { Company } from './Company';
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
-const company = new Company();
+// const user = new User();
+// const company = new Company();
 
-console.log(user);
-console.log(company);
+// console.log(user);
+// console.log(company);
+let map: HTMLElement = document.getElementById('map');
+
+new google.maps.Map(map as HTMLElement, {
+    zoom: 1, 
+    center:{
+        lat:0, lng:0
+    }
+});
